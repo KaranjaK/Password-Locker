2ee0ac68c3a007f69cfed8bab6f4750bd13b4508
KK <karanjakelvin77@gmail.com>
2022-02-22 15:10:42 +0300

    It will set up the default test for users

--- passlocker_test.py
+++ passlocker_test.py
@@ -6,4 +6,11 @@ class UsersTest (unittest.TestCase):
     '''
     Class that will define the various tests for the user class 
     '''
+    def defaultUser(self):
+        '''
+        Very first test before any other user methods test runs
+        '''
+
+        self.new_user = User('KaranjaK','K74@yea')
+
     
