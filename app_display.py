cdfe9cd811b262d6f7373a86f3a21480966a3211
KK <karanjakelvin77@gmail.com>
2022-02-22 22:37:16 +0300

    It will define a function that creates a new user

--- app_display.py
+++ app_display.py
@@ -1,3 +1,10 @@
+from hashlib import new
 from app_classes import User
 from app_classes import Credentials
 
+def new_user_crearion(username, password):
+    '''
+    To create a new user with a suer name and password
+    '''
+    new_user = User(username, password)
+    return new_user
