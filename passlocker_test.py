8bc3a0d419df3ab14d8ec0821cd1fab2e066c2a2
KK <karanjakelvin77@gmail.com>
2022-02-22 15:05:25 +0300

    It will create a test class for the User Class

--- passlocker_test.py
+++ passlocker_test.py
@@ -1,3 +1,9 @@
 import unittest
 from app_classes import User
 from app_classes import Credentials
+
+class UsersTest (unittest.TestCase):
+    '''
+    Class that will define the various tests for the user class 
+    '''
+    
