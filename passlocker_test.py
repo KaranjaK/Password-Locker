531169b1abf6f6ee46ef0e955d128b81fc9d28bd
KK <karanjakelvin77@gmail.com>
2022-02-22 15:20:40 +0300

    It will add a function to check for proper initialiation of the user

--- passlocker_test.py
+++ passlocker_test.py
@@ -6,11 +6,16 @@ class UsersTest (unittest.TestCase):
     '''
     Class that will define the various tests for the user class 
     '''
-    def defaultUser(self):
+    def default_user(self):
         '''
         Very first test before any other user methods test runs
         '''
 
         self.new_user = User('KaranjaK','K74@yea')
 
-    
+    def initialization_test(self):
+        ''''
+        Proper Initialization test
+        '''
+        self.assertEqual(self.new_user.username,'KaranjaK')
+        self.assertEqual(self.new_user.password, 'K74@yea')
