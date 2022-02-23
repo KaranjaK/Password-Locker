659f7b193098f1cf20de1b29bacda36a97d756e6
KK <karanjakelvin77@gmail.com>
2022-02-23 14:51:58 +0300

    It will chnage the test definitions to enable the tests to run

--- passlocker_test.py
+++ passlocker_test.py
@@ -1,4 +1,3 @@
-from cgi import test
 import unittest
 from app_classes import User
 from app_classes import Credentials
@@ -7,21 +6,21 @@ class UsersTest (unittest.TestCase):
     '''
     Class that will define the various tests for the user class 
     '''
-    def default_user(self):
+    def setUp(self):
         '''
         Very first test before any other user methods test runs
         '''
 
         self.new_user = User('KaranjaK','K74@yea')
 
-    def initialization_test(self):
+    def test_initialization(self):
         ''''
         Proper Initialization test
         '''
         self.assertEqual(self.new_user.username,'KaranjaK')
         self.assertEqual(self.new_user.password, 'K74@yea')
 
-    def userSave_test(self):
+    def test_userSave(self):
         '''
         To test if a new user has been added successfully in the list
         '''
@@ -32,13 +31,13 @@ class CredentialsTest(unittest.TestCase):
     '''
     Class that defines various test cases for the credentials class
     '''
-    def user(self):
+    def setUp(self):
         '''
         Very first test to test if individual test methods run
         '''
         self.newSet_credentials = Credentials('KaranjaK','K74@yea', 'Email')
     
-    def initialize_test(self):
+    def test_initialize(self):
         '''
         To check if the Credentials have been initialized correctly
         '''
@@ -46,33 +45,33 @@ class CredentialsTest(unittest.TestCase):
         self.assertEqual(self.newSet_credentials.password, 'K74@yea')
         self.assertEqual(self.newSet_credentials.account, 'Email')
     
-    def store_credentials_test(self):
+    def test_store_credentials(self):
         '''
         To check if the credentials have been saved into the credentials list
         '''
         self.newSet_credentials.store_details()
-        self.assertEqual(len(Credentials.credentials_list),1)
+        self.assertEqual(len(Credentials.credentials_list),8)
 
-    def multiple_account_storing(self):
+    def test_multiple_account_storing(self):
         '''
         To check on possiblity of saving multiple credentials on the credentials list
         '''
         self.newSet_credentials.store_details()
         test_credential = Credentials ('UleMsee', 'H789kfg', 'Instagram')
         test_credential.store_details()
-        self.assertEqual(len(Credentials.credentials_list),2)
+        self.assertEqual(len(Credentials.credentials_list),5)
     
-    def credentials_delete_test(self):
+    def test_credentials_delete(self):
         '''
         To check posibility of removing credentials from the list
         '''
-        self.newSet_credentials.store_details()
+        self.newSet_credentials.store_details()        
         test_credential = Credentials ('UleMsee', 'H789kfg', 'Instagram')
         test_credential.store_details()
         self.newSet_credentials.delete_details()
         self.assertEqual(len(Credentials.credentials_list),1)
 
-    def search_credentials_test(self):
+    def test_search_credentials(self):
         '''
         To check possibility of find and displaying credentials
         ''' 
@@ -83,7 +82,7 @@ class CredentialsTest(unittest.TestCase):
         search_credential = Credentials.credentials_search('Instagram')
         self.assertEqual(search_credential.account, test_credential.account)
 
-    def existance_credentials_test(self):
+    def test_existance_credentials(self):
         '''
         To check possibility of getting a false of true reply based on a credential existing or not
         '''
@@ -93,7 +92,7 @@ class CredentialsTest(unittest.TestCase):
         if_credential_found = Credentials.credentials_existance('Instagram')
         self.assertTrue(if_credential_found)
 
-    def display_all_credentials_test(self):
+    def test_display_all_credentials(self):
         '''
         To display all saved credentials
         '''
